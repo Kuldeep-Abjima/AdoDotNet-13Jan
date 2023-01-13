79f072b3aa10127761e0f2c200aa5e7fba2752b3
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2023-01-13 18:46:58 +0530

    Add project files.

---
+++ adonetDemo/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="adonetDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="50px" ImageUrl="C:\Users\KuldeepSingh\source\repos\AdoDotNet\AdoDotNet\img\logo.png" Width="245px" />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Height="25px" Text="Search" Width="63px" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" CellPadding="10" ForeColor="#333333" GridLines="None" Width="613px">
+                <AlternatingRowStyle BackColor="White" />
+                <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+                <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="black" />
+                <SortedAscendingCellStyle BackColor="#FDF5AC" />
+                <SortedAscendingHeaderStyle BackColor="#4D0000" />
+                <SortedDescendingCellStyle BackColor="#FCF6C0" />
+                <SortedDescendingHeaderStyle BackColor="#820000" />
+            </asp:GridView>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+    </form>
+</body>
+</html>
---
+++ adonetDemo/WebForm1.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace adonetDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    { 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            //SqlConnection con = new SqlConnection(CS);
+            //try
+            //{
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.CommandText = "select * from SuperHeros";
+            //cmd.CommandText = "select Count(id) from SuperHeros";
+            //cmd.CommandText = "insert into SuperHeros values ('Hulk', 'Anger', 'Hulk-smash', 2, 'Human')";
+            //cmd.CommandText = "update SuperHeros set origin = 'Humans' where SuperHeroId = 6";
+
+
+            //cmd.Connection = con;
+            //con.Open();
+            //var UpdatedRows = cmd.ExecuteNonQuery();
+            //Response.Write("Total Updated Rows -: " + UpdatedRows);
+
+            //var AffectedRows = cmd.ExecuteNonQuery();
+            //Response.Write("total affected rows = " + AffectedRows);
+
+            //var result = cmd.ExecuteScalar();
+            //Response.Write("Totol superheros ids are =" + result.ToString());
+            //GridView1.DataSource = cmd.ExecuteReader();
+            //GridView1.DataBind();
+
+
+
+
+
+            //}
+            //catch 
+            //{
+
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            try
+            {
+                string Command = "select * from SuperHeros where SName like '" + TextBox1.Text + "%'";
+                SqlCommand cmd = new SqlCommand(Command, con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
